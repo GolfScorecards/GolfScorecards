6f51cda3591b31a0607941766553d2ca6046bcbd
GolfScorecards <123120455+GolfScorecards@users.noreply.github.com>
2024-05-20 18:32:50 -0400

    Update added Olympic section

--- app.js
+++ app.js
@@ -431,6 +431,41 @@ function resetScorecard() {
 
 }
 
+// olympic day countdown
+
+var end = new Date('07/26/2024 12:00 AM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdownFR').innerHTML = days + 'jours ';
+    document.getElementById('countdownFR').innerHTML += hours + 'hrs ';
+    document.getElementById('countdownFR').innerHTML += minutes + 'mins ';
+    document.getElementById('countdownFR').innerHTML += seconds + 'secs';
+}
+
+timer = setInterval(showRemaining, 1000);
+
+
+
 
 ////////////////////////////////////
 
